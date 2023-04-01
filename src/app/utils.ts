0db12ffd3da55778580db0fb8b40ed2b24ec98a9
SoulDee <SoulDee@users.noreply.github.com>
2023-04-02 02:35:07 +0800

    Fix: random string a-zA-Z0-9

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,8 +3,14 @@ export const random = (min: number, max: number) => Math.round(Math.random() * (
 export const randomAsciiString = (size: number) => {
   let chars = [];
 
+  console.log(size);
+
   while (chars.length < size) {
-    chars.push(String.fromCharCode(random(0, 127)));
+    const char = String.fromCharCode(random(0, 127));
+
+    if(/[a-zA-Z0-9]/.test(char)) {
+      chars.push(char);
+    }
   }
 
   return chars.join('');
